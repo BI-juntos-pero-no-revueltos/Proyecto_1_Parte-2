c9026f39a9e0e7aa9535d356a57d8699407aaae2
AndresLugoS <a.lugos@uniandes.edu.co>
2022-11-13 11:26:54 -0500

    Modelo con regresion

--- src/main.py
+++ src/main.py
@@ -2,9 +2,14 @@ from typing import Optional
 from fastapi import FastAPI
 from joblib import load
 import pandas as pd
-import numpy as np
 from sklearn.metrics import mean_squared_error as mse
-import joblib
+import re
+from nltk.stem import WordNetLemmatizer
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction.text import TfidfTransformer
+import nltk
+nltk.download('stopwords')
+from nltk.corpus import stopwords
 
 app = FastAPI()
 
@@ -22,35 +27,66 @@ def make_predictions(LdataModel:list):
 
    rta={}
    model = load("assets/modelo.joblib")
-
+   '''
    i=1
    for x in LdataModel:
       df = pd.DataFrame(x, columns=x.keys(), index=[0])
       df.columns = ['text']
+
+      df = transform(df)
+
       result = model.predict(df)
       rta[i]=result[0]
-      i+=1 
+      i+=1       
+   '''
+   df = pd.DataFrame(LdataModel)
+   df.columns = ['text']
+   print(df)
+   df = transform(df['text'])
+
+   
+
+   result = model.predict(df)
+   
+   rta[0]=result[0]
+   rta[1]=result[1]
    return rta
 
-'''
-@app.post("/train")
-def train_model(LdataModel:list):
-   rta={}
-   model = load("assets/modelo.joblib")
 
-   df = pd.DataFrame.from_dict(LdataModel)
-   df.columns = ['Serial No.','GRE Score','TOEFL Score','University Rating','SOP',"LOR",'CGPA','Research','Admission Points']
+def transform(X):
+   documents = []
+   stemmer = WordNetLemmatizer()
+
+   for sen in range(0, len(X)):
+      # Remove all the special characters
+      document = re.sub(r'\W', ' ', str(X[sen]))
+      
+      # remove all single characters
+      document = re.sub(r'\s+[a-zA-Z]\s+', ' ', document)
+      
+      # Remove single characters from the start
+      document = re.sub(r'\^[a-zA-Z]\s+', ' ', document) 
+      
+      # Substituting multiple spaces with single space
+      document = re.sub(r'\s+', ' ', document, flags=re.I)
+      
+      # Removing prefixed 'b'
+      document = re.sub(r'^b\s+', '', document)
+      
+      # Converting to Lowercase
+      document = document.lower()
+      
+      # Lemmatization
+      document = document.split(' ')
+
+      document = [stemmer.lemmatize(word) for word in document]
+      document = ' '.join(document)
+      
+      documents.append(document)
    
-   X = df.drop('Admission Points', axis = 1)
-   y = df['Admission Points']
-   model=model.fit(X,y)
+   vectorizer = CountVectorizer(stop_words=stopwords.words('english'))
+   X = vectorizer.fit_transform(documents).toarray()
+   tfidfconverter = TfidfTransformer()
+   X = tfidfconverter.fit_transform(X).toarray()
 
-   r2=model.score(X,y)
-
-   y_true = y
-   y_predicted = model.predict(X)
-   RMSE=np.sqrt(mse(y_true, y_predicted))
-
-   joblib.dump(model,"assets/modelo.joblib")
-   return {"R^2": r2, "RMSE": RMSE}
-'''
+   return X
