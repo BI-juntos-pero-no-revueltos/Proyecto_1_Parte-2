6fb92becc81c7d236317a649ab89d5e75e4edf39
LuisaFuentesL <l.fuentesl@uniandes.edu.co>
2022-11-13 16:36:23 -0500

    el moni nos ayudo

---
+++ src/Limpieza.py
@@ -0,0 +1,40 @@
+import re
+from sklearn.base import BaseEstimator
+from nltk.stem import WordNetLemmatizer
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction.text import TfidfTransformer
+import nltk
+from sklearn.datasets import load_files
+nltk.download('stopwords')
+from nltk.corpus import stopwords
+
+
+def Limpieza(text):
+    documents = []
+    stemmer = WordNetLemmatizer()
+
+    # Remove all the special characters
+    document = re.sub(r'\W', ' ', str(text))
+    
+    # remove all single characters
+    document = re.sub(r'\s+[a-zA-Z]\s+', ' ', document)
+    
+    # Remove single characters from the start
+    document = re.sub(r'\^[a-zA-Z]\s+', ' ', document) 
+    
+    # Substituting multiple spaces with single space
+    document = re.sub(r'\s+', ' ', document, flags=re.I)
+    
+    # Removing prefixed 'b'
+    document = re.sub(r'^b\s+', '', document)
+    
+    # Converting to Lowercase
+    document = document.lower()
+    
+    # Lemmatization
+    document = document.split(' ')
+
+    document = [stemmer.lemmatize(word) for word in document]
+    document = ' '.join(document)
+
+    return document
--- src/main.py
+++ src/main.py
@@ -8,6 +8,7 @@ from nltk.stem import WordNetLemmatizer
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.feature_extraction.text import TfidfTransformer
 import nltk
+from Limpieza import Limpieza
 nltk.download('stopwords')
 from nltk.corpus import stopwords
 
@@ -26,67 +27,26 @@ def read_item(item_id: int, q: Optional[str] = None):
 def make_predictions(LdataModel:list):
 
    rta={}
-   model = load("assets/modelo.joblib")
+   model = load("vectorizador.joblib")
+ 
    '''
    i=1
    for x in LdataModel:
       df = pd.DataFrame(x, columns=x.keys(), index=[0])
-      df.columns = ['text']
 
-      df = transform(df)
+      df['text'] = df['text'].apply(Limpieza)
 
       result = model.predict(df)
       rta[i]=result[0]
-      i+=1       
-   '''
+      i+=1   
+   '''   
    df = pd.DataFrame(LdataModel)
-   df.columns = ['text']
    print(df)
-   df = transform(df['text'])
-
-   
-
-   result = model.predict(df)
-   
-   rta[0]=result[0]
-   rta[1]=result[1]
+   df['text'] = df['text'].apply(Limpieza)
+   print(df)
+   result = model.predict(df['text']) 
+   print(result)
+ 
    return rta
 
 
-def transform(X):
-   documents = []
-   stemmer = WordNetLemmatizer()
-
-   for sen in range(0, len(X)):
-      # Remove all the special characters
-      document = re.sub(r'\W', ' ', str(X[sen]))
-      
-      # remove all single characters
-      document = re.sub(r'\s+[a-zA-Z]\s+', ' ', document)
-      
-      # Remove single characters from the start
-      document = re.sub(r'\^[a-zA-Z]\s+', ' ', document) 
-      
-      # Substituting multiple spaces with single space
-      document = re.sub(r'\s+', ' ', document, flags=re.I)
-      
-      # Removing prefixed 'b'
-      document = re.sub(r'^b\s+', '', document)
-      
-      # Converting to Lowercase
-      document = document.lower()
-      
-      # Lemmatization
-      document = document.split(' ')
-
-      document = [stemmer.lemmatize(word) for word in document]
-      document = ' '.join(document)
-      
-      documents.append(document)
-   
-   vectorizer = CountVectorizer(stop_words=stopwords.words('english'))
-   X = vectorizer.fit_transform(documents).toarray()
-   tfidfconverter = TfidfTransformer()
-   X = tfidfconverter.fit_transform(X).toarray()
-
-   return X
